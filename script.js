df49b5556b59f7cd5a9e96b6784288af1a471f14
Akshay-k-1810 <akshayk1810@gmail.com>
2023-09-19 13:31:20 +0530

    commit

--- script.js
+++ script.js
@@ -1,77 +1,70 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-let ready = false
-let imagesLoaded = 0
-let totalImages = 0
-let photosArray = []
-
-let count = 5
-const apiKey = '9bMX2ziheUAmmwoy0uZC1Pe8p-zKjCDQe7OlaQCq5Ns'
-const apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`
-
+let isReady = false;
+let imagesLoadedCount = 0;
+let totalImagesCount = 0;
+let photosArray = [];
+var offset = 0; // Initial offset
 function imageLoaded() {
-  imagesLoaded++
-
-  if(imagesLoaded === totalImages) {
-    ready = true;
-    loader.hidden = true
-    count = 30
+  imagesLoadedCount++;
+  if (imagesLoadedCount === totalImagesCount) {
+    isReady = true;
+    loader.style.display = 'none'; // Hide the loader when all images are loaded
   }
 }
 
-function setAttributes(element, attributes) {
-  for (const key in attributes) {
-    element.setAttribute(key, attributes[key])
-  }
+function createImageElement(photo) {
+  const imageItem = document.createElement('div');
+  imageItem.classList.add('image-item');
+
+  const img = document.createElement('img');
+  img.src = photo.url;
+  img.alt = photo.description;
+
+  imageItem.appendChild(img);
+
+  imageContainer.appendChild(imageItem);
 }
 
-// Create Elements for links & Photos, Add to DOM
-function showPhotos() {
-  imagesLoaded = 0
-  totalImages = photosArray.length
-
-  photosArray.forEach((photo) => {
-    const item = document.createElement('a');
-
-    setAttributes(item, {
-      href: photo.links.html,
-      target: '_blank'
-    })
-
-    const img = document.createElement('img');
-
-    setAttributes(img, {
-      src: photo.urls.regular,
-      alt: photo.alt_description,
-    })
-
-    img.addEventListener('load', imageLoaded)
-
-    item.appendChild(img);
-    imageContainer.appendChild(item);
-  })
-}
-
-
-async function retrievePhotos() {
+async function fetchPhotos() {
   try {
-    const response = await fetch(apiURL)
-    photosArray = await response.json(); // returns an array
-    showPhotos();
+    const response = await fetch(
+      `https://api.slingacademy.com/v1/sample-data/photos?offset=${offset}&limit=5`
+    );
+    offset += 5; // Increment the offset by 5
+    const data = await response.json();
 
+    if (!data.success) {
+      throw new Error(data.message);
+    }
+
+    const newPhotosArray = data.photos;
+    photosArray = [...photosArray, ...newPhotosArray];
+    totalImagesCount = photosArray.length;
+
+    newPhotosArray.forEach((photo) => {
+      createImageElement(photo);
+    });
+
+    isReady = true;
+    loader.style.display = 'none'; // Hide the loader when all images are loaded
   } catch (error) {
-      alert('Error in Retrieving Images')
+    alert(`Error in Retrieving Images: ${error.message}`);
   }
 }
 
-// Check to see if scrolling near bottom of page, Load more photos
+// Check to see if scrolling near the bottom of the page, load more photos
 window.addEventListener('scroll', () => {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
-    ready = false
-    getPhotos()
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 1000 &&
+    isReady
+  ) {
+    isReady = false;
+    fetchPhotos();
   }
-})
+});
 
 // On Load
-retrievePhotos();
+fetchPhotos();
